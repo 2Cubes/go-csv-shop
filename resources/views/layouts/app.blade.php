b9f14aa124faa05fc2632224a5055a2c0e9a548d
2cubes <order@2cubes.ru>
2024-09-17 15:48:58 +1000

    Slugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
     <title>@yield('title')</title>
     <meta name="description" content="@yield('description')">
     <!-- Bootstrap icons-->
